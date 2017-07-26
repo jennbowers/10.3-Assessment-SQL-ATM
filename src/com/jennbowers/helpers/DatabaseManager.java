77dbbd5014720269c48f98ae21ea9a9bc5d7ad03
Jenn Bowers <jenniferbowers03@gmail.com>
2017-07-26 17:12:27 -0400

    Successfully connected to model and database manager classes. Can now implement find all method.

--- src/com/jennbowers/helpers/DatabaseManager.java
+++ src/com/jennbowers/helpers/DatabaseManager.java
@@ -1,6 +1,7 @@
 package com.jennbowers.helpers;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -14,12 +15,22 @@ public class DatabaseManager {
         this.statement = connection.createStatement();
     }
 
+    public Statement getStatement() {
+        return statement;
+    }
+
     public void createAccountTable() throws SQLException {
-        statement.executeUpdate("CREATE TABLE account (id INTEGER PRIMARY KEY, transactionAmount STRING, user STRING)");
+        statement.executeUpdate("CREATE TABLE account (id INTEGER PRIMARY KEY, transactionAmount STRING)");
 
     }
 
     public void dropAccountTable() throws SQLException {
         statement.executeUpdate("DROP TABLE IF EXISTS account");
+    }
+
+    public ResultSet findAll (String table) throws SQLException {
+        String formattedSql = String.format("SELECT * FROM %s", table);
+        ResultSet rs = statement.executeQuery(formattedSql);
+        return rs;
     }
 }
--- src/com/jennbowers/model/Account.java
+++ src/com/jennbowers/model/Account.java
@@ -1,7 +1,56 @@
 package com.jennbowers.model;
 
+import com.jennbowers.helpers.DatabaseManager;
+
+import javax.xml.transform.Result;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jenniferbowers on 7/26/17.
  */
 public class Account {
+    private int id;
+    private double transactionAmount;
+    private Statement statement;
+
+//    constructor
+    public Account(double transactionAmount, Statement statement) {
+        this.transactionAmount = transactionAmount;
+        this.statement = statement;
+    }
+
+//    overloaded constructor
+    public Account(double transactionAmount, Statement statement, int id) {
+        this(transactionAmount, statement);
+        this.id = id;
+    }
+
+    public void save() throws SQLException{
+        String formattedSql = String.format("INSERT INTO account (transactionAmount) VALUES (%s)", transactionAmount);
+        statement.executeUpdate(formattedSql);
+    }
+
+    public static List<Account> findAll(DatabaseManager dbm) throws SQLException{
+        ResultSet rs = dbm.findAll("account");
+        List<Account> tempCollection = new ArrayList<>();
+        Statement tempStatement = dbm.getStatement();
+
+        while(rs.next()) {
+            double transactionAmount = rs.getDouble("transactionAmount");
+            Account tempAccount = new Account(transactionAmount, tempStatement, rs.getInt("id"));
+            tempCollection.add(tempAccount);
+        }
+        return tempCollection;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "transactionAmount=" + transactionAmount +
+                '}';
+    }
 }
